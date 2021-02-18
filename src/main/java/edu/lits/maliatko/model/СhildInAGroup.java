e21fd552994cce159b8fd8bb0e71368c6fb840c6
VladimirKing <cheremuha1@gmail.com>
2021-02-18 20:11:06 +0200

    group list, add, delete

---
+++ src/main/java/edu/lits/maliatko/model/СhildInAGroup.java
@@ -0,0 +1,76 @@
+package edu.lits.maliatko.model;
+
+public class СhildInAGroup {
+
+    String name;
+    String surname;
+    String dateOfBirdth;
+    int id;
+
+    public СhildInAGroup() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDateOfBirdth() {
+        return dateOfBirdth;
+    }
+
+    public void setDateOfBirdth(String dateOfBirdth) {
+        this.dateOfBirdth = dateOfBirdth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "СhildInAGroup{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirdth='" + dateOfBirdth + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        СhildInAGroup that = (СhildInAGroup) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        return dateOfBirdth != null ? dateOfBirdth.equals(that.dateOfBirdth) : that.dateOfBirdth == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (dateOfBirdth != null ? dateOfBirdth.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
+    }
+}
