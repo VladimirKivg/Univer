3d25f1fbdbe06d1b182f9dd22802f1f49842567d
VladimirKing <cheremuha1@gmail.com>
2021-02-19 09:19:16 +0200

    mm

--- src/main/java/edu/lits/maliatko/controller/СhildInAGroupController.java
+++ src/main/java/edu/lits/maliatko/controller/СhildInAGroupController.java
@@ -3,6 +3,7 @@ package edu.lits.maliatko.controller;
 import edu.lits.maliatko.model.СhildInAGroup;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -12,26 +13,31 @@ import java.util.List;
 @RequestMapping("/childInAGroup")
 public class СhildInAGroupController {
 
-
-    @RequestMapping("/list")
-    public String list(ModelMap model) {
-
-        List<СhildInAGroup> childInAGroup = getСhildInAGroup();
-        model.addAttribute("childInAGroupList", childInAGroup);
-
-        model.addAttribute("content", "childInAGroupList");
-        return "index";
-    }
-    private List<СhildInAGroup> getСhildInAGroup() {
-        List<СhildInAGroup> getСhildInList = new ArrayList<>();
+    private static List<СhildInAGroup> getСhildInAGroup = new ArrayList<>();
+    static {
         СhildInAGroup child = new СhildInAGroup();
         child.setName("Маленький");
         child.setSurname("Малюк");
         child.setDateOfBirdth("24.13.2022");
         child.setId(1);
-        getСhildInList.add(child);
+        getСhildInAGroup.add(child);
         // add other groups
-        return getСhildInList;
+    }
+
+    @RequestMapping("/list")
+    public String list(ModelMap model) {
+
+        List<СhildInAGroup> childInAGroup = getСhildInAGroup;
+        model.addAttribute("childInAGroupList", childInAGroup);
+
+        model.addAttribute("content", "childInAGroupList");
+        return "index";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id, ModelMap model) {
+        getСhildInAGroup.remove(id-1);
+        return "redirect:/childInAGroup/list";
     }
 
 
