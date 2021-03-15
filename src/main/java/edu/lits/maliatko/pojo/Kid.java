60675584560933c71778544df8edf8e0cd3790fe
Serhiikon <kondrserg@gmail.com>
2021-03-15 21:23:42 +0200

    створив папки pojo, repository, додав залежності в файл build.gradle

---
+++ src/main/java/edu/lits/maliatko/pojo/Kid.java
@@ -0,0 +1,23 @@
+package edu.lits.maliatko.pojo;
+
+import java.util.Date;
+
+public class Kid {
+    private Integer id;
+    private String surname;
+    private String name;
+    private String fatherName;
+    private Date birthDate;
+    private String gender;
+    private String birthDocument;
+    private Integer addressId;
+    private Integer parentOneId;
+    private String benefits;
+    private String status;
+    private Integer parentTwoId;
+    private Integer medicalDataId;
+    private Date registerToGardenDate;
+    private Integer groupId;
+
+
+}
