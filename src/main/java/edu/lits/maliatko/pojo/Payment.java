f7c54fceff35a29fe49a85aa8cf8797f7362b556
VladimirKing <cheremuha1@gmail.com>
2021-03-16 00:21:01 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/Payment.java
+++ src/main/java/edu/lits/maliatko/pojo/Payment.java
@@ -1,2 +1,14 @@
-package edu.lits.maliatko.pojo;public class Payment {
+package edu.lits.maliatko.pojo;
+
+import java.util.Date;
+
+public class Payment {
+    private Integer id;//id
+    private Date date;//date
+    private String bankPaymentNumber;//bank_payment_number
+    private String description;//description
+    private String paymentCol;//paymentcol
+    private Double payAmount;//pay_amount
+    private Date registerDate;//register_date
+
 }
