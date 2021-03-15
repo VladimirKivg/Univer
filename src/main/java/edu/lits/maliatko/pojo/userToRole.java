c522e887facc302e9bff04ec0d48464815f3c0a6
VladimirKing <cheremuha1@gmail.com>
2021-03-16 00:23:01 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/userToRole.java
+++ src/main/java/edu/lits/maliatko/pojo/userToRole.java
@@ -1,2 +1,7 @@
-package edu.lits.maliatko.pojo;public class userToRole {
+package edu.lits.maliatko.pojo;
+
+public class userToRole {//user_to_role
+    private Integer id;//id
+    private Integer userId;//user_id
+    private Integer roleId;//role_id
 }
