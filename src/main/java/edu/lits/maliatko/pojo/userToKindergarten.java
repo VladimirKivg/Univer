49939f9ee94b3faa75b2891d8be6d04e0fa431a0
VladimirKing <cheremuha1@gmail.com>
2021-03-16 00:22:31 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/userToKindergarten.java
+++ src/main/java/edu/lits/maliatko/pojo/userToKindergarten.java
@@ -1,2 +1,8 @@
-package edu.lits.maliatko.pojo;public class userToKindergarten {
+package edu.lits.maliatko.pojo;
+
+public class userToKindergarten {//user_to_kindergarten
+    private Integer id;//id
+    private Integer userId;//user_id
+    private Integer kindergartenId;//kindergarten_id
+
 }
