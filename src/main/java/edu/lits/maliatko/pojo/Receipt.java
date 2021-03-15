ab51e9f5ebf8535ec46381e3663db23f8116a638
VladimirKing <cheremuha1@gmail.com>
2021-03-16 00:21:30 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/Receipt.java
+++ src/main/java/edu/lits/maliatko/pojo/Receipt.java
@@ -1,2 +1,14 @@
-package edu.lits.maliatko.pojo;public class Receipt {
+package edu.lits.maliatko.pojo;
+
+import java.util.Date;
+
+public class Receipt {
+    private Integer id;//id
+    private Integer kidId;//kid_id
+    private Integer visitedDays;//visited_days
+    private Double costPerDay;//cost_per_day
+    private Double debtOverpay;//debt_overpay
+    private Double sumToPay;//sum_to_pay
+    private Date payPeriod;//pay_period
+    private Integer paymentId;//payment_id
 }
