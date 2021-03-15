f161a4760bf5519187b6f75d208bc58427597f9e
Serhiikon <kondrserg@gmail.com>
2021-03-15 22:26:11 +0200

    додав класи в pojo: Address, Visiting

---
+++ src/main/java/edu/lits/maliatko/pojo/Address.java
@@ -0,0 +1,10 @@
+package edu.lits.maliatko.pojo;
+
+public class Address {
+    private Integer id; //`id`
+    private String region; //`region`
+    private String city; //`city`
+    private String street; //`street`
+    private Integer buildingNumber; //`building_number`
+    private Integer apartment; //`apartment`
+}
---
+++ src/main/java/edu/lits/maliatko/pojo/Visiting.java
@@ -0,0 +1,11 @@
+package edu.lits.maliatko.pojo;
+
+import java.util.Date;
+
+public class Visiting {
+    private Integer id; // `id`
+    private Integer kidId; // `kid_id`
+    private Date visitingDate; // `visiting_date`
+    private Integer presence; // `presence`
+    private Integer userLoggerId; // `user_logger_id`
+}
