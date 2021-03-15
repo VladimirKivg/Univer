b886b7c63ccbccbf1f0ded54fe9836ea5175977d
VladimirKing <cheremuha1@gmail.com>
2021-03-16 00:22:09 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/User.java
+++ src/main/java/edu/lits/maliatko/pojo/User.java
@@ -1,12 +1,21 @@
 package edu.lits.maliatko.pojo;
 
+import org.hibernate.query.criteria.internal.expression.function.CurrentDateFunction;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 public class User {
    private Integer id;
    private String surname;
    private String name;
    private String fatherName;//father_name
-   private
-   private
-   private
-   private
+   private Date birthDate;
+   private String phone;
+   private String mail;
+   private Integer passportId;
+   private Integer addressId;
+   private String login;
+   private String password;
 }
