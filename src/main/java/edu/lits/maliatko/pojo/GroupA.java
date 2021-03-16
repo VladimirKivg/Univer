96555615b8ab05130d60599f56db7a5bb7ac4943
Serhiikon <kondrserg@gmail.com>
2021-03-16 19:29:31 +0200

    додав папку для репозиторіїв

--- src/main/java/edu/lits/maliatko/pojo/GroupA.java
+++ src/main/java/edu/lits/maliatko/pojo/GroupA.java
@@ -1,6 +1,6 @@
 package edu.lits.maliatko.pojo;
 
-public class Group1 {
+public class GroupA {
     private Integer id;  // id
     private String name;  // name
     private Integer kindergartenId; // kindergarten_id
---
+++ src/main/java/edu/lits/maliatko/repository/AddressRepository.java
@@ -0,0 +1,4 @@
+package edu.lits.maliatko.repository;
+
+public interface AddressRepository {
+}
