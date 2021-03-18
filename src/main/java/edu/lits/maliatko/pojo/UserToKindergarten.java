0ef7c0c18c07da443f0cc5621269ab64fcda6b91
VladimirKing <cheremuha1@gmail.com>
2021-03-19 01:01:03 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/UserToKindergarten.java
+++ src/main/java/edu/lits/maliatko/pojo/UserToKindergarten.java
@@ -5,15 +5,58 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 
 @Entity(name = "user_to_kindergarten")
-public class userToKindergarten {//user_to_kindergarten
+public class UserToKindergarten {//user_to_kindergarten
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
     @GenericGenerator(name = "native",strategy ="native" )
     private Integer id;//id
 
-//    @ManyToOne()
-    private User userId;//user_id
-//    @ManyToOne()
-    private Kindergarten kindergartenId;//kindergarten_id
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User userId;
 
+    @ManyToOne()
+    @JoinColumn(name = "kindergarten_id")
+    private Kindergarten kindergartenId;
+
+    public UserToKindergarten() {
+    }
+
+    public UserToKindergarten(User userId, Kindergarten kindergartenId) {
+        this.userId = userId;
+        this.kindergartenId = kindergartenId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Kindergarten getKindergartenId() {
+        return kindergartenId;
+    }
+
+    public void setKindergartenId(Kindergarten kindergartenId) {
+        this.kindergartenId = kindergartenId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserToKindergarten{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", kindergartenId=" + kindergartenId +
+                '}';
+    }
 }
