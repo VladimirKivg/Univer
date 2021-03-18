9e287b9e6fc5af5828c24b0a1dbb7f1e1e54ef19
VladimirKing <cheremuha1@gmail.com>
2021-03-19 01:01:27 +0200

    m

--- src/main/java/edu/lits/maliatko/pojo/UserToGroup.java
+++ src/main/java/edu/lits/maliatko/pojo/UserToGroup.java
@@ -6,14 +6,57 @@ import javax.persistence.*;
 
 @Entity(name = "user_to_group")
 public class UserToGroup {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
     @GenericGenerator(name = "native",strategy ="native" )
     private Integer id;    // id
 
-//    @ManyToOne
-    private User userId;   // user_id
-//    @ManyToOne
-    private GroupA groupId;   // group_id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GroupA groupId;
 
+    public UserToGroup() {
+    }
+
+    public UserToGroup(User userId, GroupA groupId) {
+        this.userId = userId;
+        this.groupId = groupId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public GroupA getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(GroupA groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserToGroup{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", groupId=" + groupId +
+                '}';
+    }
 }
