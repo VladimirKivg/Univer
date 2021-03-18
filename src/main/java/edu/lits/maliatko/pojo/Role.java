d60fe954a86de819d61003f333bc8336b063a0b6
VladimirKing <cheremuha1@gmail.com>
2021-03-19 01:03:11 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/Role.java
+++ src/main/java/edu/lits/maliatko/pojo/Role.java
@@ -8,11 +8,43 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
     @GenericGenerator(name = "native",strategy ="native" )
-    private Integer id;   //id
-    @Column(name = "role")
-    private String role;   // role
+    private Integer id;
 
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
