6ab6e9e92226ab92901fd17c9a48f8a6ca368820
VladimirKing <cheremuha1@gmail.com>
2021-03-19 01:00:38 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/UserToRole.java
+++ src/main/java/edu/lits/maliatko/pojo/UserToRole.java
@@ -3,18 +3,61 @@ package edu.lits.maliatko.pojo;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+
 @Entity(name = "user_to_role")
-public class userToRole {//user_to_role
+public class UserToRole {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
-    @GenericGenerator(name = "native",strategy ="native" )
-    private Integer id;//id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Integer id;
 
-    /*  @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id") */
-    private User userId;//user_id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User userId;
 
-    /*  @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "role_id") */
-    private Role roleId;//role_id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id")
+    private Role roleId;
+
+    public UserToRole() {
+    }
+
+    public UserToRole(User userId, Role roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Role getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Role roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserToRole{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", roleId=" + roleId +
+                '}';
+    }
 }
