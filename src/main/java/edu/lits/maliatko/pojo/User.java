32434ed4b8bdde77ccbc78eb1b578959b44d5f5b
VladimirKing <cheremuha1@gmail.com>
2021-03-19 01:02:16 +0200

    mm

--- src/main/java/edu/lits/maliatko/pojo/User.java
+++ src/main/java/edu/lits/maliatko/pojo/User.java
@@ -7,33 +7,164 @@ import org.springframework.dao.DataAccessResourceFailureException;
 import javax.persistence.*;
 import javax.xml.crypto.Data;
 import java.util.Date;
+
 @Entity(name = "user")
 public class User {
-   @Id
-   @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
-   @GenericGenerator(name = "native",strategy ="native" )
-   private Integer id;
-   @Column(name = "surname")
-   private String surname;
-   @Column(name = "name")
-   private String name;
-   @Column(name = "father_name")
-   private String fatherName;//father_name
-   @Column(name = "birth_date")
-   private Date birthDate;
-   @Column(name = "phone")
-   private String phone;
-   @Column(name = "mail")
-   private String mail;
-   @Column(name = "passport_id")
-   private Integer passportId;
 
-   /*  @ManyToOne(cascade = CascadeType.ALL)
-   @JoinColumn(name = "address_id") */
-   private Address addressId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Integer id;
 
-   @Column(name = "login")
-   private String login;
-   @Column(name = "password")
-   private String password;
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "father_name")
+    private String fatherName;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "passport_id")
+    private Integer passportId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address addressId;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String surname, String name, String fatherName, Date birthDate, String phone, String mail, Integer passportId, Address addressId, String login, String password) {
+        this.surname = surname;
+        this.name = name;
+        this.fatherName = fatherName;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.mail = mail;
+        this.passportId = passportId;
+        this.addressId = addressId;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Integer getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(Integer passportId) {
+        this.passportId = passportId;
+    }
+
+    public Address getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Address addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", fatherName='" + fatherName + '\'' +
+                ", birthDate=" + birthDate +
+                ", phone='" + phone + '\'' +
+                ", mail='" + mail + '\'' +
+                ", passportId=" + passportId +
+                ", addressId=" + addressId +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
