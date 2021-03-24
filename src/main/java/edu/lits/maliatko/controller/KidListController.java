d493e002aa73a46b37fe124f86991cbef019c903
VladimirKing <cheremuha1@gmail.com>
2021-03-24 21:42:55 +0200

    m

--- src/main/java/edu/lits/maliatko/controller/KidListController.java
+++ src/main/java/edu/lits/maliatko/controller/KidListController.java
@@ -1,2 +1,37 @@
-package edu.lits.maliatko.controller;public class KidListController {
+package edu.lits.maliatko.controller;
+
+// цей клас писав Вова
+import edu.lits.maliatko.pojo.Kid;
+import edu.lits.maliatko.repository.KidRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class KidListController {
+private KidRepository kidRepository;
+
+
+
+    @RequestMapping("/list-kind")
+    public String listKinder(Model model){
+        List<Kid> kidList=new ArrayList<>();
+//        Iterable<Kid> kidIterable ;
+//        kidIterable=kidRepository.findAll();
+//        for (Kid kid:kidIterable) {
+//            kidList.add(kid);
+//        }
+        Kid kid =new Kid();
+        kid.setName("Вова");
+        kid.setSurname("Великий");
+        kid.setFatherName("Володимирович");
+        kidList.add(kid);
+        model.addAttribute("attributeKidList",kidList);
+model.addAttribute("content","listKind");
+        return "index";
+    }
+
 }
