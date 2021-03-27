480edc84ce78a295ef92a1e2670c0ab4943cb02e
VladimirKing <cheremuha1@gmail.com>
2021-03-27 02:57:33 +0200

    змшна KidList на Queue

--- src/main/java/edu/lits/maliatko/controller/QueueController.java
+++ src/main/java/edu/lits/maliatko/controller/QueueController.java
@@ -3,33 +3,35 @@ package edu.lits.maliatko.controller;
 // цей клас писав Вова
 import edu.lits.maliatko.pojo.Child;
 import edu.lits.maliatko.repository.ChildRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-public class KidListController {
-private ChildRepository kidRepository;
+public class QueueController {
 
+    @Autowired
+private ChildRepository childRepository;
 
 
     @RequestMapping("/list-kind")
     public String listKinder(Model model){
-        List<Child> kidList=new ArrayList<>();
-//        Iterable<Kid> kidIterable ;
-//        kidIterable=kidRepository.findAll();
-//        for (Kid kid:kidIterable) {
-//            kidList.add(kid);
-//        }
-        Child kid =new Child();
-        kid.setName("Вова");
-        kid.setSurname("Великий");
-        kid.setFatherName("Володимирович");
-        kidList.add(kid);
-        model.addAttribute("attributeKidList",kidList);
+        Iterable<Child> all = childRepository.findAll();
+        List<Child>childList=new ArrayList<>();
+        for (Child child:all){
+            childList.add(child);
+        }
+//        Child child =new Child();
+//        child.setName("Вова");
+//        child.setSurname("Великий");
+//        child.setFatherName("Володимирович");
+
+        model.addAttribute("attributeKidList",childList);
 model.addAttribute("content","listKind");
         return "index";
     }
