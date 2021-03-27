c8c98ec4338157eb11b200cf94002d47e2c6b180
VladimirKing <cheremuha1@gmail.com>
2021-03-27 02:53:58 +0200

    зміна Group на Cluster

--- src/main/java/edu/lits/maliatko/repository/ClusterRepository.java
+++ src/main/java/edu/lits/maliatko/repository/ClusterRepository.java
@@ -3,5 +3,5 @@ package edu.lits.maliatko.repository;
 import edu.lits.maliatko.pojo.Cluster;
 import org.springframework.data.repository.CrudRepository;
 
-interface GroupARepository  extends CrudRepository <Cluster,Integer>{
+interface ClusterRepository extends CrudRepository <Cluster,Integer>{
 }
