d471b1249a18eddc90c9244ed57bc7295cb9acac
VladimirKing <cheremuha1@gmail.com>
2021-03-29 19:03:59 +0300

    інформація про дитину

--- src/main/java/edu/lits/maliatko/controller/InfoKidController.java
+++ src/main/java/edu/lits/maliatko/controller/InfoKidController.java
@@ -8,36 +8,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import java.util.Optional;
 
 @Controller
 public class InfoKidController {
-    @Autowired
-    private QueueRepository kidListRepository;
+
+
     @Autowired
     private ChildRepository kidRepository;
 
+    @RequestMapping("/info-kid-id")
+    public String infirmKind1(Model model){
+        model.addAttribute("content","kidInfoId");
+    return "index";}
+
+
+
     @RequestMapping("/info-kid")
-    public String informKind(Model model) {
-        Queue kidList = new Queue();
-        Child child = kidRepository.findById(11).get();
-        Optional<Child> byId = kidRepository.findById(11);
+    public String informKind(@RequestParam(value = "id")String idStr, Model model) {
 
-//child.setFatherName("Б");
-//child.setName("и");
-//child.setSurname("З");
-        String name = child.getName();
-        String surname = child.getSurname();
-        String fatherName = child.getFatherName();
+        try {
+            int id=Integer.parseInt(idStr);
+        Child child = kidRepository.findById(id).get();
 
-        model.addAttribute("name", name);
-        model.addAttribute("surname", surname);
-        model.addAttribute("fatherName", fatherName);
+        model.addAttribute("child", child);
 
         model.addAttribute("content","kidInfo");
 //kidIfo
-        return "index";
-    }
+        } catch (Exception e) {
+            String message="дитини за таким ID не знайдено, поверніться будьласка в попереднє меню і спробуйте ще раз";
+
+            model.addAttribute("message", message);
+            model.addAttribute("content","kidInfoNot");
+            return "index";}
+        return "index";}
 
 }
