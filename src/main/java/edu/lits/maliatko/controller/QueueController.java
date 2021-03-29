e60ac4863475ba01a2261482357c5608874b588d
VladimirKing <cheremuha1@gmail.com>
2021-03-29 19:08:11 +0300

    черга

--- src/main/java/edu/lits/maliatko/controller/QueueController.java
+++ src/main/java/edu/lits/maliatko/controller/QueueController.java
@@ -2,7 +2,10 @@ package edu.lits.maliatko.controller;
 
 // цей клас писав Вова
 import edu.lits.maliatko.pojo.Child;
+import edu.lits.maliatko.pojo.Cluster;
+import edu.lits.maliatko.pojo.Queue;
 import edu.lits.maliatko.repository.ChildRepository;
+import edu.lits.maliatko.repository.QueueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,28 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Controller
 public class QueueController {
 
-    @Autowired
-private ChildRepository childRepository;
 
+@Autowired
+    private QueueRepository queueRepository;
 
-    @RequestMapping("/list-kind")
+    @RequestMapping("/queue")
     public String listKinder(Model model){
-        Iterable<Child> all = childRepository.findAll();
+        Iterable<Queue> all1 = queueRepository.findAll();
         List<Child>childList=new ArrayList<>();
-        for (Child child:all){
-            childList.add(child);
+        for (Queue queue:all1){
+            childList.add(queue.getKidId());
         }
-//        Child child =new Child();
-//        child.setName("Вова");
-//        child.setSurname("Великий");
-//        child.setFatherName("Володимирович");
+//        for (Child child:childList){
+//            clusterList.add(child.getCluster());
+//        }
+
 
         model.addAttribute("attributeKidList",childList);
+//        model.addAttribute("attributeClusterList",clusterList);
 model.addAttribute("content","listKind");
         return "index";
     }
