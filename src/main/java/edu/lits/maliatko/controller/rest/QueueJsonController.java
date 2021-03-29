152fcd37bd80b61d1923e8eff035d17fa48e1cb4
VladimirKing <cheremuha1@gmail.com>
2021-03-29 19:08:43 +0300

    черга

--- src/main/java/edu/lits/maliatko/controller/rest/QueueJsonController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/QueueJsonController.java
@@ -1,2 +1,31 @@
-package edu.lits.maliatko.controller.rest;public class QueueJsonController {
+package edu.lits.maliatko.controller.rest;
+
+import edu.lits.maliatko.pojo.Child;
+import edu.lits.maliatko.pojo.Queue;
+import edu.lits.maliatko.repository.QueueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/json")
+public class QueueJsonController {
+
+    @Autowired
+    private QueueRepository queueRepository;
+
+    @RequestMapping("/queue")
+    public List<Queue> listKinder(Model model){
+        Iterable<Queue> all1 = queueRepository.findAll();
+        List<Queue> childList=new ArrayList<>();
+        all1.forEach(childList::add);
+
+        return childList;
+    }
+
 }
+
