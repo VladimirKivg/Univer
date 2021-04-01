5c90cc405bead1b3296a3eb18078da7e9f65f184
Tatiana <tatianagreenchuk@gmail.com>
2021-04-02 00:14:05 +0300

    class ClusterModel

---
+++ src/main/java/edu/lits/maliatko/model/ClusterModel.java
@@ -0,0 +1,18 @@
+package edu.lits.maliatko.model;
+
+public class ClusterModel {
+
+
+    private String name;
+
+    public ClusterModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
