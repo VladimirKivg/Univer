4fd38418340d6dfab6989419e0105722521ca751
VladimirKing <cheremuha1@gmail.com>
2021-04-04 00:25:20 +0300

    educator ще доробляється

--- src/main/java/edu/lits/maliatko/model/EducatorModel.java
+++ src/main/java/edu/lits/maliatko/model/EducatorModel.java
@@ -1,2 +1,41 @@
-package edu.lits.maliatko.model;public class EducatorModel {
+package edu.lits.maliatko.model;
+
+public class EducatorModel {
+
+    String name;
+    String surname;
+    String fatherName;
+
+    public EducatorModel() {
+    }
+
+    public EducatorModel(String name, String surname, String fatherName) {
+        this.name = name;
+        this.surname = surname;
+        this.fatherName = fatherName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
 }
