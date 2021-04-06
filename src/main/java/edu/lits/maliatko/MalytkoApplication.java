c82e3a7486648ecbdac59223d4c0e018a75bae4f
Tatiana <tatianagreenchuk@gmail.com>
2021-04-06 20:42:27 +0300

    Підключила бібліотеки і все,що було в презентаціі.

--- src/main/java/edu/lits/maliatko/MalytkoApplication.java
+++ src/main/java/edu/lits/maliatko/MalytkoApplication.java
@@ -3,9 +3,11 @@ package edu.lits.maliatko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "edu.lits.maliatko.controller" } )
+@EnableSwagger2
 public class MalytkoApplication {
 
 	public static void main(String[] args) {
