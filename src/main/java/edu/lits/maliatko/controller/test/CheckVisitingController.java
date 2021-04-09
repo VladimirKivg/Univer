cd763378e9526dd434a3b71e301d29b2e6fa32d4
VladimirKing <cheremuha1@gmail.com>
2021-04-09 20:46:32 +0300

    перевірка чи всі відмічені

--- src/main/java/edu/lits/maliatko/controller/test/CheckVisitingController.java
+++ src/main/java/edu/lits/maliatko/controller/test/CheckVisitingController.java
@@ -1,2 +1,52 @@
-package edu.lits.maliatko.controller.test;public class CheckVisitingController {
+package edu.lits.maliatko.controller.test;
+
+import edu.lits.maliatko.pojo.Child;
+import edu.lits.maliatko.pojo.Visiting;
+import edu.lits.maliatko.repository.ChildRepository;
+import edu.lits.maliatko.repository.VisitingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Controller
+public class CheckVisitingController {
+
+    @Autowired
+VisitingRepository visitingRepository;
+
+    @Autowired
+    ChildRepository childRepository;
+
+    @RequestMapping("/visitingCheck")
+    public String visiting(Model model){
+        Iterable<Visiting> all = visitingRepository.findAll();
+        List<Visiting>visitingList=new ArrayList<>();
+        all.forEach(visitingList::add);
+
+        Iterable<Child> all2 = childRepository.findAll();
+        List<Child>childList=new ArrayList<>();
+        all2.forEach(childList::add);
+
+        List<Child>childList2=new ArrayList<>();
+        for (Visiting visiting:all){
+            childList2.add(visiting.getChild());
+        }
+
+        childList.removeAll(childList2);
+
+        Iterable<Visiting> allByPresence = visitingRepository.findAllByPresence(null);
+List<Visiting>visitingList2=new ArrayList<>();
+allByPresence.forEach(visitingList2::add);
+
+        model.addAttribute("attributeChildList2",visitingList2);
+        model.addAttribute("attributeChildList",childList);
+        model.addAttribute("content","checkVisiting");
+    return "index";}
+
 }
