caac473ba1076e409cca752d5efc8160b648eea7
VladimirKing <cheremuha1@gmail.com>
2021-04-11 02:07:16 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/test/UserTestController.java
+++ src/main/java/edu/lits/maliatko/controller/test/UserTestController.java
@@ -1,5 +1,6 @@
 package edu.lits.maliatko.controller.test;
 
+import edu.lits.maliatko.model.test.UserModel;
 import edu.lits.maliatko.pojo.User;
 import edu.lits.maliatko.repository.UserRepository;
 
@@ -12,20 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserTestController {
 
-@Autowired
-    UserRepository userRepository;
+//@Autowired
+//UserRepository userRepository;
 
 @Autowired
     UserService userService;
 
     @RequestMapping("/userTest")
     public String userTest(Model model) {
-        userService.findUserById();
-        User user=userRepository.findById(1).get();
-        String adres=user.getAddress().getApartment();
-        String name = user.getName();
+        UserModel userModel = userService.findUserById(1);
+
+
+//       User user=userRepository.findById(1).get();
+//        String adres=user.getAddress().getApartment();
+//        String name = user.getName();
+
+        String name=userModel.getName();
          model.addAttribute("name",name);
-         model.addAttribute("apartment",adres);
+//         model.addAttribute("apartment",adres);
          model.addAttribute("content","TESTuser");
 
         return "index";
--- src/main/java/edu/lits/maliatko/service/UserServiceImpl.java
+++ src/main/java/edu/lits/maliatko/service/UserServiceImpl.java
@@ -1,12 +1,22 @@
 package edu.lits.maliatko.service;
 
 import edu.lits.maliatko.model.test.UserModel;
+import edu.lits.maliatko.pojo.User;
+import edu.lits.maliatko.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+   private UserRepository userRepository;
+
     @Override
     public UserModel findUserById(Integer id) {
-        return null;
+        User user = userRepository.findById(id).get();
+
+        UserModel userModel= new UserModel(user.getId(), user.getName());
+
+        return userModel;
     }
 }
