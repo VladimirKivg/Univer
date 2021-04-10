519d78616424f17d79e909e5dcab0303867676a5
VladimirKing <cheremuha1@gmail.com>
2021-04-10 19:19:59 +0300

    прибираю лишнє

--- src/main/java/edu/lits/maliatko/controller/RegistrationToKindergartenController.java
+++ src/main/java/edu/lits/maliatko/controller/RegistrationToKindergartenController.java
@@ -64,9 +64,6 @@ public class RegistrationToKindergartenController {
 
         Child child = new Child(kidReg.getKidSurname(), kidReg.getKidName(), kidReg.getKidFatherName(), kidReg.getKidBirthDate(), kidReg.getKidGender(), kidReg.getKidBirthDocument(), kidAddress, userParentOne, kidReg.getKidBenefits(), "в черзі", null, 0, date, null);
         childRepository.save(child);
-        // child.addressId = kidAddress.id
-        // зберегти обєкт child
-
 
         Kindergarten kindergarten = new Kindergarten();
         Iterable<Kindergarten> all = kindergartenRepository.findAll();
