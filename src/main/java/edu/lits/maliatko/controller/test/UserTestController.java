633df1e5548bfb3d554fcc0197d55f57a44a58cc
VladimirKing <cheremuha1@gmail.com>
2021-04-11 01:32:31 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/test/UserTestController.java
+++ src/main/java/edu/lits/maliatko/controller/test/UserTestController.java
@@ -3,6 +3,7 @@ package edu.lits.maliatko.controller.test;
 import edu.lits.maliatko.pojo.User;
 import edu.lits.maliatko.repository.UserRepository;
 
+import edu.lits.maliatko.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +15,12 @@ public class UserTestController {
 @Autowired
     UserRepository userRepository;
 
+@Autowired
+    UserService userService;
+
     @RequestMapping("/userTest")
     public String userTest(Model model) {
+        userService.findUserById();
         User user=userRepository.findById(1).get();
         String adres=user.getAddress().getApartment();
         String name = user.getName();
