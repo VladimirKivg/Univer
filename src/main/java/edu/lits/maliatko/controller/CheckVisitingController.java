86072b5c96a189f9ce6722e8ba5ad8f0b00d0852
VladimirKing <cheremuha1@gmail.com>
2021-04-13 21:41:47 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/CheckVisitingController.java
+++ src/main/java/edu/lits/maliatko/controller/CheckVisitingController.java
@@ -1,9 +1,11 @@
 package edu.lits.maliatko.controller;
 
+import edu.lits.maliatko.model.test.CheckVisitingModel;
 import edu.lits.maliatko.pojo.Child;
 import edu.lits.maliatko.pojo.Visiting;
 import edu.lits.maliatko.repository.ChildRepository;
 import edu.lits.maliatko.repository.VisitingRepository;
+import edu.lits.maliatko.service.CheckVisitingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,35 +19,22 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Controller
 public class CheckVisitingController {
 
-    @Autowired
-VisitingRepository visitingRepository;
+//    @Autowired
+//VisitingRepository visitingRepository;
 
     @Autowired
-    ChildRepository childRepository;
+    private CheckVisitingService checkVisitingService;
 
     @RequestMapping("/visitingCheck")
     public String visiting(Model model){
-        Iterable<Visiting> all = visitingRepository.findAll();
-        List<Visiting>visitingList=new ArrayList<>();
-        all.forEach(visitingList::add);
+        List<CheckVisitingModel> allByPresence1 = checkVisitingService.findAllByPresence(null);
 
-        Iterable<Child> all2 = childRepository.findAll();
-        List<Child>childList=new ArrayList<>();
-        all2.forEach(childList::add);
+//        Iterable<Visiting> allByPresence = visitingRepository.findAllByPresence(null);
+//List<Visiting>visitingList2=new ArrayList<>();
+//allByPresence.forEach(visitingList2::add);
 
-        List<Child>childList2=new ArrayList<>();
-        for (Visiting visiting:all){
-            childList2.add(visiting.getChild());
-        }
+        model.addAttribute("attributeChildList2",allByPresence1);
 
-        childList.removeAll(childList2);
-
-        Iterable<Visiting> allByPresence = visitingRepository.findAllByPresence(null);
-List<Visiting>visitingList2=new ArrayList<>();
-allByPresence.forEach(visitingList2::add);
-
-        model.addAttribute("attributeChildList2",visitingList2);
-        model.addAttribute("attributeChildList",childList);
         model.addAttribute("content","checkVisiting");
     return "index";}
 
