3830ef60569f90f71dc21dcfac6835878514b429
VladimirKing <cheremuha1@gmail.com>
2021-04-13 22:34:02 +0300

    service

--- src/main/java/edu/lits/maliatko/service/CheckVisitingService.java
+++ src/main/java/edu/lits/maliatko/service/CheckVisitingService.java
@@ -1,2 +1,11 @@
-package edu.lits.maliatko.service;public interface CheckVisitingService {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.test.CheckVisitingModel;
+import edu.lits.maliatko.pojo.Visiting;
+
+import java.util.List;
+
+public interface CheckVisitingService {
+
+    List<CheckVisitingModel> findAllByPresence(Integer presence);
 }
