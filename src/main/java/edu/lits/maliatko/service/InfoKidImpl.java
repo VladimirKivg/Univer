7e21a52784dc4bcfc20fdc3f294ed49eb5c6cbfa
VladimirKing <cheremuha1@gmail.com>
2021-04-15 02:00:17 +0300

    work from infoKid and Service

--- src/main/java/edu/lits/maliatko/service/InfoKidImpl.java
+++ src/main/java/edu/lits/maliatko/service/InfoKidImpl.java
@@ -1,15 +1,22 @@
 package edu.lits.maliatko.service;
 
 import edu.lits.maliatko.model.test.InfoKidModel;
+import edu.lits.maliatko.pojo.Child;
+import edu.lits.maliatko.repository.ChildRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InfoKidImpl implements InfoKidService{
 
-
+    @Autowired
+private ChildRepository childRepository;
 
     @Override
     public InfoKidModel findById(Integer id) {
+        Child child = childRepository.findById(id).get();
         return null;
     }
 }
