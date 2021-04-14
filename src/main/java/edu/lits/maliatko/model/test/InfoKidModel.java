499ca8742637107ec45b52fd8149641c527e8301
VladimirKing <cheremuha1@gmail.com>
2021-04-15 01:46:13 +0300

    work from infoKid and Service

--- src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
+++ src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
@@ -3,13 +3,9 @@ package edu.lits.maliatko.model.test;
 import edu.lits.maliatko.service.InfoKidService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class InfoKidModel implements InfoKidService {
+
+public class InfoKidModel  {
 
 
 
-    @Override
-    public InfoKidModel findById(Integer id) {
-        return null;
-    }
 }
