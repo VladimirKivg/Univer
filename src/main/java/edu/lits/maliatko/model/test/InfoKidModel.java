191988b25c35629c3c9a22ed4c8422ea526e2c5b
VladimirKing <cheremuha1@gmail.com>
2021-04-15 02:04:40 +0300

    work from infoKid and service

--- src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
+++ src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
@@ -1,11 +1,67 @@
 package edu.lits.maliatko.model.test;
 
+import edu.lits.maliatko.pojo.Address;
+import edu.lits.maliatko.pojo.Cluster;
+import edu.lits.maliatko.pojo.User;
 import edu.lits.maliatko.service.InfoKidService;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.*;
+import java.util.Date;
 
 
 public class InfoKidModel  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Integer id;
 
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "father_name")
+    private String fatherName;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "birth_document")
+    private String birthDocument;
+
+    @ManyToOne()
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne()
+    @JoinColumn(name = "parent_one_id")
+    private User userParentOne;
+
+    @Column(name = "benefits")
+    private String benefits;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne()
+    @JoinColumn(name = "parent_two_id",nullable = true)
+    private User userParentTwo;
+
+    @Column(name = "medical_data_id")
+    private Integer medicalDataId;
+
+    @Column(name = "register_to_garden_date")
+    private Date registerToGardenDate;
+
+    @ManyToOne()
+    @JoinColumn(name = "cluster_id",nullable = true)
+    private Cluster cluster;
 
 
 }
