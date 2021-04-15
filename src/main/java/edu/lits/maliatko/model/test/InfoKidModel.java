99d5259ead156ad08144a4355fe01f23380b6ad8
VladimirKing <cheremuha1@gmail.com>
2021-04-15 03:35:26 +0300

    finish infoKid and Service

--- src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
+++ src/main/java/edu/lits/maliatko/model/test/InfoKidModel.java
@@ -3,65 +3,126 @@ package edu.lits.maliatko.model.test;
 import edu.lits.maliatko.pojo.Address;
 import edu.lits.maliatko.pojo.Cluster;
 import edu.lits.maliatko.pojo.User;
-import edu.lits.maliatko.service.InfoKidService;
-import org.hibernate.annotations.GenericGenerator;
-import org.springframework.stereotype.Service;
 
-import javax.persistence.*;
 import java.util.Date;
 
 
 public class InfoKidModel  {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+
     private Integer id;
-
-    @Column(name = "surname")
     private String surname;
-
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "father_name")
     private String fatherName;
-
-    @Column(name = "birth_date")
     private Date birthDate;
-
-    @Column(name = "gender")
-    private String gender;
-
-    @Column(name = "birth_document")
-    private String birthDocument;
-
-    @ManyToOne()
-    @JoinColumn(name = "address_id")
     private Address address;
-
-    @ManyToOne()
-    @JoinColumn(name = "parent_one_id")
     private User userParentOne;
-
-    @Column(name = "benefits")
     private String benefits;
-
-    @Column(name = "status")
     private String status;
-
-    @ManyToOne()
-    @JoinColumn(name = "parent_two_id",nullable = true)
     private User userParentTwo;
-
-    @Column(name = "medical_data_id")
-    private Integer medicalDataId;
-
-    @Column(name = "register_to_garden_date")
-    private Date registerToGardenDate;
-
-    @ManyToOne()
-    @JoinColumn(name = "cluster_id",nullable = true)
     private Cluster cluster;
 
+    public InfoKidModel() {
+    }
 
+    public InfoKidModel(Integer id, String surname, String name, String fatherName, Date birthDate, Address address, User userParentOne, String benefits, String status, User userParentTwo, Cluster cluster) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.fatherName = fatherName;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.userParentOne = userParentOne;
+        this.benefits = benefits;
+        this.status = status;
+        this.userParentTwo = userParentTwo;
+        this.cluster = cluster;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public User getUserParentOne() {
+        return userParentOne;
+    }
+
+    public void setUserParentOne(User userParentOne) {
+        this.userParentOne = userParentOne;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUserParentTwo() {
+        return userParentTwo;
+    }
+
+    public void setUserParentTwo(User userParentTwo) {
+        this.userParentTwo = userParentTwo;
+    }
+
+    public Cluster getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(Cluster cluster) {
+        this.cluster = cluster;
+    }
 }
