30f69d68d8bc933c464dca28a14533564e44af42
marko <public.marko.pyrizhok@gmail.com>
2021-04-15 20:30:50 +0300

    add interaction to queue page

--- src/main/java/edu/lits/maliatko/repository/QueueRepository.java
+++ src/main/java/edu/lits/maliatko/repository/QueueRepository.java
@@ -1,7 +1,14 @@
 package edu.lits.maliatko.repository;
 
+import edu.lits.maliatko.pojo.Kindergarten;
 import edu.lits.maliatko.pojo.Queue;
+import edu.lits.maliatko.pojo.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QueueRepository extends CrudRepository<Queue, Integer> {
+
+
+    Iterable<Queue> findByKindergarten_Name(String kindergartenName);
+
+    Iterable<Queue> findByKindergartenId(Integer kindergarten_id);
 }
