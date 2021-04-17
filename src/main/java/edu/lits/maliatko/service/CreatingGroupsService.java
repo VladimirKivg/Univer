0a3a9f2ce964b39bf27b8d75966b8679dbcbaa79
VladimirKing <cheremuha1@gmail.com>
2021-04-17 21:24:40 +0300

    service

--- src/main/java/edu/lits/maliatko/service/CreatingGroupsService.java
+++ src/main/java/edu/lits/maliatko/service/CreatingGroupsService.java
@@ -1,2 +1,11 @@
-package edu.lits.maliatko.service;public interface CreatingGroupsService {
+package edu.lits.maliatko.service;
+
+
+import edu.lits.maliatko.model.CreatingGroupsModel;
+import edu.lits.maliatko.pojo.Cluster;
+
+public interface CreatingGroupsService {
+ //   void save(Cluster cluster);
+
+    void save(CreatingGroupsModel creatingGroups);
 }
