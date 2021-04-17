0f9d7764b3c1f6afb221872d0f18a60e14a0a3a7
VladimirKing <cheremuha1@gmail.com>
2021-04-17 21:23:37 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/CreatingGroupsController.java
+++ src/main/java/edu/lits/maliatko/controller/CreatingGroupsController.java
@@ -1,37 +1,34 @@
 package edu.lits.maliatko.controller;
 
 
-import edu.lits.maliatko.model.CreatingGroups;
-import edu.lits.maliatko.pojo.Cluster;
-import edu.lits.maliatko.repository.ClusterRepository;
+import edu.lits.maliatko.model.CreatingGroupsModel;
+
+import edu.lits.maliatko.service.CreatingGroupsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CreatingGroupsController {
 
+
     @Autowired
-    private ClusterRepository clusterRepository;
+    private CreatingGroupsService creatingGroupsService;
 
     @RequestMapping("/creating_groups")
-    public String creating(CreatingGroups creatingGroups, ModelMap model) {
+    public String creating(CreatingGroupsModel creatingGroups, ModelMap model) {
         model.addAttribute("content", "creatingGroups");
         return "index";
     }
 
 
     @RequestMapping(value = "/addNameGroup", method = RequestMethod.POST)
-    public String addNameGroups(CreatingGroups creatingGroups) {
+    public String addNameGroups(CreatingGroupsModel creatingGroups) {
 
-        Cluster cluster = new Cluster(creatingGroups.getNameGroup(), creatingGroups.getKindergartenId());
-
-        clusterRepository.save(cluster);
-
+        //цей метод в CreatingGroupImpl з creatingGroups заповнюється Cluster і встановлює в б.д
+        creatingGroupsService.save(creatingGroups);
 
         return "redirect:/management";
     }
