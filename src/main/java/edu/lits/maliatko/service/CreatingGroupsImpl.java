29ae8af32a3ace0ad37521a18a3914ebf63b219d
VladimirKing <cheremuha1@gmail.com>
2021-04-17 21:24:01 +0300

    service

--- src/main/java/edu/lits/maliatko/service/CreatingGroupsImpl.java
+++ src/main/java/edu/lits/maliatko/service/CreatingGroupsImpl.java
@@ -1,2 +1,28 @@
-package edu.lits.maliatko.service;public class CreatingGroupsImpl {
+package edu.lits.maliatko.service;
+
+
+import edu.lits.maliatko.model.CreatingGroupsModel;
+import edu.lits.maliatko.pojo.Cluster;
+import edu.lits.maliatko.repository.ClusterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreatingGroupsImpl implements CreatingGroupsService{
+
+    @Autowired
+   private ClusterRepository clusterRepository;
+
+
+
+    @Override
+    public void save(CreatingGroupsModel creatingGroups) {
+        Cluster cluster = new Cluster(creatingGroups.getNameGroup(), creatingGroups.getKindergartenId());
+        clusterRepository.save(cluster);
+    }
+
+//    @Override
+//    public void save(Cluster cluster) {
+//        clusterRepository.save(cluster);
+//    }
 }
