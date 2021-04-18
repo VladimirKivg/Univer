43f71938acb1c675e38e16cd4ea9ad90418c7b15
VladimirKing <cheremuha1@gmail.com>
2021-04-18 20:39:03 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/GroupController.java
+++ src/main/java/edu/lits/maliatko/controller/GroupController.java
@@ -1,9 +1,11 @@
 package edu.lits.maliatko.controller;
 
+import edu.lits.maliatko.model.ClusterModel;
 import edu.lits.maliatko.model.Group;
 import edu.lits.maliatko.pojo.Cluster;
 import edu.lits.maliatko.pojo.Payment;
 import edu.lits.maliatko.repository.ClusterRepository;
+import edu.lits.maliatko.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,55 +22,55 @@ import java.util.List;
 @Controller
 @RequestMapping("/group")
 public class GroupController {
-    @Autowired
-    private ClusterRepository clusterRepository;
 
+    @Autowired
+    private GroupService groupService;
 
     @RequestMapping("/list")
     public String list(ModelMap model) {
-        Iterable<Cluster> groupList = clusterRepository.findAll();
-        model.addAttribute("groups", groupList);
+        List<ClusterModel> clusterModelList = groupService.findAll();
+        model.addAttribute("groups", clusterModelList);
         model.addAttribute("content", "groupList");
         return "index";
     }
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable("id") Integer id, ModelMap model) {
-        clusterRepository.deleteById(id);
+        groupService.deleteById(id);
         return "redirect:/group/list";
     }
 
-    @RequestMapping("/add-group")
-    public String addGroup(Group group, ModelMap model, BindingResult result) {
-        model.addAttribute("content", "createGroup");
-        return "index";
-    }
+//    @RequestMapping("/add-group")
+//    public String addGroup(Group group, ModelMap model, BindingResult result) {
+//        model.addAttribute("content", "createGroup");
+//        return "index";
+//    }
 
-    /*@RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String add(@Valid Group group , BindingResult result, ModelMap model) {
-        if (result.hasErrors()) {
-            model.addAttribute("content", "createGroup");
-            return "index";
-        }
-        groupList.add(group);
-        return "redirect:/group/list";
-    }
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public String add(@Valid Group group , BindingResult result, ModelMap model) {
+//        if (result.hasErrors()) {
+//            model.addAttribute("content", "createGroup");
+//            return "index";
+//        }
+//        groupList.add(group);
+//        return "redirect:/group/list";
+//    }
 
-    @RequestMapping("/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        Group group = groupList.get(0); // this logic is not correct, group has to be                        //                                    selected by it’s id.
-        model.addAttribute("group", group);
-        model.addAttribute("content", "updateGroup");
-        return "index";
-    }
-    @RequestMapping("/update/{id}")
-    public String updateGroup(@PathVariable("id") int id, Group group,
-                              BindingResult result, Model model) {
+//    @RequestMapping("/edit/{id}")
+//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+//        Group group = groupList.get(0); // this logic is not correct, group has to be                        //                                    selected by it’s id.
+//        model.addAttribute("group", group);
+//        model.addAttribute("content", "updateGroup");
+//        return "index";
+//    }
+//    @RequestMapping("/update/{id}")
+//    public String updateGroup(@PathVariable("id") int id, Group group,
+//                              BindingResult result, Model model) {
+//
+//        groupList.get(id).setName(group.getName());
+//        groupList.get(id).setNumber(group.getNumber());
+//        return "redirect:/group/list";
+//    }
 
-        groupList.get(id).setName(group.getName());
-        groupList.get(id).setNumber(group.getNumber());
-        return "redirect:/group/list";
-    }
-*/
 
 }
