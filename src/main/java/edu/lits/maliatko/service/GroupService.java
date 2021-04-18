f3296a8ff10815c76eea1da3322d96327f9097da
VladimirKing <cheremuha1@gmail.com>
2021-04-18 20:40:54 +0300

    service

--- src/main/java/edu/lits/maliatko/service/GroupService.java
+++ src/main/java/edu/lits/maliatko/service/GroupService.java
@@ -1,2 +1,12 @@
-package edu.lits.maliatko.service;public interface GroupService {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.ClusterModel;
+import edu.lits.maliatko.pojo.Cluster;
+
+import java.util.List;
+
+public interface GroupService {
+    List<ClusterModel> findAll();
+
+    void deleteById(Integer id);
 }
