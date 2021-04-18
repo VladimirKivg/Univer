f863f927b8da737fbaa9b0b921777baa5828ef6f
VladimirKing <cheremuha1@gmail.com>
2021-04-18 20:39:24 +0300

    service

--- src/main/java/edu/lits/maliatko/service/GroupImpl.java
+++ src/main/java/edu/lits/maliatko/service/GroupImpl.java
@@ -1,2 +1,33 @@
-package edu.lits.maliatko.service;public class GroupImpl {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.ClusterModel;
+import edu.lits.maliatko.pojo.Cluster;
+import edu.lits.maliatko.repository.ClusterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GroupImpl implements GroupService{
+
+    @Autowired
+    private ClusterRepository clusterRepository;
+
+    @Override
+    public List<ClusterModel> findAll() {
+        Iterable<Cluster> all = clusterRepository.findAll();
+        List<ClusterModel>clusterModelList=new ArrayList<>();
+        for (Cluster cluster:all){
+          ClusterModel clusterModel = new ClusterModel(cluster.getId(),cluster.getName(),cluster.getKindergarten());
+          clusterModelList.add(clusterModel);
+        }
+        return clusterModelList;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+clusterRepository.deleteById(id);
+    }
 }
