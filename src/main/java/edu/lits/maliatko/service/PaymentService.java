5a1d8e0cb588ef76335e0992cc06733da582f991
VladimirKing <cheremuha1@gmail.com>
2021-04-19 11:52:47 +0300

    переробляю на service

--- src/main/java/edu/lits/maliatko/service/PaymentService.java
+++ src/main/java/edu/lits/maliatko/service/PaymentService.java
@@ -1,2 +1,12 @@
-package edu.lits.maliatko.service;public interface PaymentService {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.PaymentModel;
+import edu.lits.maliatko.pojo.Payment;
+
+import java.util.List;
+
+public interface PaymentService {
+    List<PaymentModel> findAll();
+
+    void deleteById(Integer id);
 }
