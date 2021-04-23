1d518cc096fac33b9f2322007979c7f28674c801
VladimirKing <cheremuha1@gmail.com>
2021-04-23 22:50:15 +0300

    service

--- src/main/java/edu/lits/maliatko/model/KindergartenModel.java
+++ src/main/java/edu/lits/maliatko/model/KindergartenModel.java
@@ -1,2 +1,56 @@
-package edu.lits.maliatko.model;public class KindergartenModel {
+package edu.lits.maliatko.model;
+
+import edu.lits.maliatko.pojo.Address;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+public class KindergartenModel {
+
+    private Integer id;
+    private Integer number;
+    private String name;
+    private Address address;
+
+    public KindergartenModel() {
+    }
+
+    public KindergartenModel( Integer number, String name, Address address) {
+
+        this.number = number;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
