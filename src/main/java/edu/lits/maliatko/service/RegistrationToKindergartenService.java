8e748707848ec04db9f67f7141f417e93fe44732
VladimirKing <cheremuha1@gmail.com>
2021-04-23 22:52:41 +0300

    service

--- src/main/java/edu/lits/maliatko/service/RegistrationToKindergartenService.java
+++ src/main/java/edu/lits/maliatko/service/RegistrationToKindergartenService.java
@@ -1,2 +1,22 @@
-package edu.lits.maliatko.service;public interface RegistrationToKindergartenServise {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.KidReg;
+import edu.lits.maliatko.model.KindergartenModel;
+
+
+import java.util.List;
+
+public interface RegistrationToKindergartenService {
+
+    void saveKidAddress(KidReg kidReg);
+
+    void saveUserAddress(KidReg kidReg);
+
+    void saveUserParentOne(KidReg kidReg);
+
+    void saveChild(KidReg kidReg);
+
+    List<KindergartenModel> findAllKindergarten();
+
+    void saveQueue(KidReg kidReg);
 }
