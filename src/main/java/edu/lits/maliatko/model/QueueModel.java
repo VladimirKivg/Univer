5677347b172733889ce5091aa1bafe88b02e7372
VladimirKing <cheremuha1@gmail.com>
2021-04-23 22:51:29 +0300

    service

--- src/main/java/edu/lits/maliatko/model/QueueModel.java
+++ src/main/java/edu/lits/maliatko/model/QueueModel.java
@@ -1,2 +1,58 @@
-package edu.lits.maliatko.model;public class QueueModel {
+package edu.lits.maliatko.model;
+
+import edu.lits.maliatko.pojo.Child;
+import edu.lits.maliatko.pojo.Kindergarten;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class QueueModel {
+
+    private Integer id;
+    private Child child;
+    private Kindergarten kindergarten;
+    private Date applyDate;
+
+    public QueueModel() {
+    }
+
+    public QueueModel(Integer id, Child child, Kindergarten kindergarten, Date applyDate) {
+        this.id = id;
+        this.child = child;
+        this.kindergarten = kindergarten;
+        this.applyDate = applyDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public Kindergarten getKindergarten() {
+        return kindergarten;
+    }
+
+    public void setKindergarten(Kindergarten kindergarten) {
+        this.kindergarten = kindergarten;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
 }
