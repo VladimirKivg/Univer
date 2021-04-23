a623c64d8f277ae344643570536a9b3a940b8e20
VladimirKing <cheremuha1@gmail.com>
2021-04-23 22:51:39 +0300

    service

--- src/main/java/edu/lits/maliatko/service/QueueService.java
+++ src/main/java/edu/lits/maliatko/service/QueueService.java
@@ -1,2 +1,14 @@
-package edu.lits.maliatko.service;public interface Queue {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.KindergartenModel;
+import edu.lits.maliatko.model.QueueModel;
+
+
+import java.util.List;
+
+public interface QueueService {
+    List<KindergartenModel> findByName(String kindergartenName);
+
+    List<QueueModel> findByKindergarten_Name(String kindergartenName);
+
 }
