601c2bc7d9ad8d87a192ac2088caa3d39784ea62
VladimirKing <cheremuha1@gmail.com>
2021-04-23 22:52:19 +0300

    service

--- src/main/java/edu/lits/maliatko/controller/RegistrationToKindergartenController.java
+++ src/main/java/edu/lits/maliatko/controller/RegistrationToKindergartenController.java
@@ -5,6 +5,7 @@ import edu.lits.maliatko.model.KidReg;
 import edu.lits.maliatko.pojo.*;
 import edu.lits.maliatko.repository.*;
 
+import edu.lits.maliatko.service.RegistrationToKindergartenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -20,24 +21,26 @@ import java.util.Optional;
 @Controller
 public class RegistrationToKindergartenController {
 
-
     @Autowired
-    private AddressRepository addressRepository;
+private RegistrationToKindergartenService service;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ChildRepository childRepository;
-
-    @Autowired
-    private KindergartenRepository kindergartenRepository;
-
-    @Autowired
-    private QueueRepository queueRepository;
-
-    @Autowired
-    private ClusterRepository clusterRepository;
+//    @Autowired
+//    private AddressRepository addressRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private ChildRepository childRepository;
+//
+//    @Autowired
+//    private KindergartenRepository kindergartenRepository;
+//
+//    @Autowired
+//    private QueueRepository queueRepository;
+//
+//    @Autowired
+//    private ClusterRepository clusterRepository;
 
     @RequestMapping("/registration")
     public String registration(ModelMap model) {
@@ -48,31 +51,47 @@ public class RegistrationToKindergartenController {
     @RequestMapping(value = "/addKidToList", method = RequestMethod.POST)
     public String addKidToList(KidReg kidReg) {
 
-        Address kidAddress = new Address(kidReg.getKidRegion(), kidReg.getKidCity(), kidReg.getKidStreet(),
-                kidReg.getKidBuildingNumber(), kidReg.getKidApartment());
-        addressRepository.save(kidAddress);
+//        Address kidAddress = new Address(kidReg.getKidRegion(), kidReg.getKidCity(), kidReg.getKidStreet(),
+//                kidReg.getKidBuildingNumber(), kidReg.getKidApartment());
+//        addressRepository.save(kidAddress);
 
-        Address userAddress = new Address(kidReg.getParentRegion(), kidReg.getParentCity(), kidReg.getParentStreet(), kidReg.getParentBuildingNumber(), kidReg.getParentApartment());
+        service.saveKidAddress(kidReg);
 
-        addressRepository.save(userAddress);
 
-        User userParentOne = new User(kidReg.getParentSurname(), kidReg.getParentName(), kidReg.getParentFatherName(), kidReg.getParentBirthDate(), kidReg.getParentPhone(), kidReg.getParentMail(), userAddress);
-        userRepository.save(userParentOne);
+//        Address userAddress = new Address(kidReg.getParentRegion(), kidReg.getParentCity(), kidReg.getParentStreet(), kidReg.getParentBuildingNumber(), kidReg.getParentApartment());
+//        addressRepository.save(userAddress);
 
-        Date date = new Date();
+        service.saveUserAddress(kidReg);
 
-        Child child = new Child(kidReg.getKidSurname(), kidReg.getKidName(), kidReg.getKidFatherName(), kidReg.getKidBirthDate(), kidReg.getKidGender(), kidReg.getKidBirthDocument(), kidAddress, userParentOne, kidReg.getKidBenefits(), "в черзі", null, 0, date, null);
-        childRepository.save(child);
 
-        Kindergarten kindergarten = new Kindergarten();
-        Iterable<Kindergarten> all = kindergartenRepository.findAll();
-        for (Kindergarten kid : all) {
-            if (Integer.parseInt(kidReg.getKindergartenName()) == kid.getNumber()) {
-                kindergarten = kid;
-            }
-        }
-        Queue queue = new Queue(child, kindergarten, date);
-        queueRepository.save(queue);
+//        User userParentOne = new User(kidReg.getParentSurname(), kidReg.getParentName(), kidReg.getParentFatherName(), kidReg.getParentBirthDate(), kidReg.getParentPhone(), kidReg.getParentMail(), userAddress);
+//        userRepository.save(userParentOne);
+
+        service.saveUserParentOne(kidReg);
+
+
+//        Date date = new Date();
+//
+//        Child child = new Child(kidReg.getKidSurname(), kidReg.getKidName(), kidReg.getKidFatherName(), kidReg.getKidBirthDate(), kidReg.getKidGender(), kidReg.getKidBirthDocument(), kidAddress, userParentOne, kidReg.getKidBenefits(), "в черзі", null, 0, date, null);
+//        childRepository.save(child);
+
+       service.saveChild(kidReg);
+
+
+//        Kindergarten kindergarten = new Kindergarten();
+//        Iterable<Kindergarten> all = kindergartenRepository.findAll();
+//
+//        service.findAllKindergarten();
+//
+//        for (Kindergarten kid : all) {
+//            if (Integer.parseInt(kidReg.getKindergartenName()) == kid.getNumber()) {
+//                kindergarten = kid;
+//            }
+//        }
+//        Queue queue = new Queue(child, kindergarten, date);
+//        queueRepository.save(queue);
+
+        service.saveQueue(kidReg);
 
         return "redirect:/queue";
     }
