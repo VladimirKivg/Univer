3bab5843b5fc63c07d551c0159c5144f8f5c9d10
VladimirKing <cheremuha1@gmail.com>
2021-04-30 17:08:08 +0300

    /json/kindergartenList

--- src/main/java/edu/lits/maliatko/controller/rest/KindergartenListJsonController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/KindergartenListJsonController.java
@@ -1,2 +1,27 @@
-package edu.lits.maliatko.controller.rest;public class KindergartenListJsonController {
+package edu.lits.maliatko.controller.rest;
+
+import edu.lits.maliatko.pojo.Kindergarten;
+import edu.lits.maliatko.repository.KindergartenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/json")
+public class KindergartenListJsonController {
+
+    @Autowired
+    private KindergartenRepository kindergartenRepository;
+
+    @RequestMapping("/kindergartenList")
+   public List<Kindergarten> kindergartenList(){
+        Iterable<Kindergarten> all = kindergartenRepository.findAll();
+        List<Kindergarten>kindergartenList=new ArrayList<>();
+        all.forEach(kindergartenList::add);
+
+        return kindergartenList;
+   }
 }
