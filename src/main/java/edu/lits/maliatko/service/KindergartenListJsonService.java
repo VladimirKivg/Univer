996f29dbb9e1f0af2ef91bf76a360e88be832031
VladimirKing <cheremuha1@gmail.com>
2021-04-30 17:57:36 +0300

    Service /json/kindergartenList

--- src/main/java/edu/lits/maliatko/service/KindergartenListJsonService.java
+++ src/main/java/edu/lits/maliatko/service/KindergartenListJsonService.java
@@ -1,2 +1,10 @@
-package edu.lits.maliatko.service;public class KindergartenListJsonService {
+package edu.lits.maliatko.service;
+
+import edu.lits.maliatko.model.KindergartenModel;
+import edu.lits.maliatko.pojo.Kindergarten;
+
+import java.util.List;
+
+public interface KindergartenListJsonService {
+    public List<KindergartenModel> findAll();
 }
