a6d37a0702e4563e2fb1362936a8fdd3598dc867
VladimirKing <cheremuha1@gmail.com>
2021-04-30 18:36:05 +0300

    Service /json/kindergartenList

--- src/main/java/edu/lits/maliatko/service/KindergartenListJsonImpl.java
+++ src/main/java/edu/lits/maliatko/service/KindergartenListJsonImpl.java
@@ -1,12 +1,30 @@
 package edu.lits.maliatko.service;
 
 import edu.lits.maliatko.model.KindergartenModel;
+import edu.lits.maliatko.pojo.Kindergarten;
+import edu.lits.maliatko.repository.KindergartenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class KindergartenListJsonImpl implements KindergartenListJsonService{
+
+    @Autowired
+   private KindergartenRepository kindergartenRepository;
+
     @Override
     public List<KindergartenModel> findAll() {
-        return null;
+
+        Iterable<Kindergarten> all = kindergartenRepository.findAll();
+        List<KindergartenModel>kindergartenModelList=new ArrayList<>();
+        for (Kindergarten kindergarten :all){
+            KindergartenModel kindergartenModel=new KindergartenModel(kindergarten.getNumber(), kindergarten.getName(), kindergarten.getAddress());
+            kindergartenModel.setId(kindergarten.getId());
+            kindergartenModelList.add(kindergartenModel);
+        }
+
+        return kindergartenModelList;
     }
 }
