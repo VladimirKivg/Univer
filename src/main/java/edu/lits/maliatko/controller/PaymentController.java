b4f7676068e2dd7fd25ead444c8424f943774166
VladimirKing <cheremuha1@gmail.com>
2021-05-06 16:43:52 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/controller/PaymentController.java
+++ src/main/java/edu/lits/maliatko/controller/PaymentController.java
@@ -1,16 +1,12 @@
 package edu.lits.maliatko.controller;
 
 import edu.lits.maliatko.model.PaymentModel;
-import edu.lits.maliatko.pojo.Payment;
-import edu.lits.maliatko.repository.PaymentRepository;
 import edu.lits.maliatko.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
