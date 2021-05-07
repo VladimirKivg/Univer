9dcc598a893971c0429e506870845b94f80a3fc7
VladimirKing <cheremuha1@gmail.com>
2021-05-07 13:16:15 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/repository/QueueRepository.java
+++ src/main/java/edu/lits/maliatko/repository/QueueRepository.java
@@ -1,8 +1,6 @@
 package edu.lits.maliatko.repository;
 
-import edu.lits.maliatko.pojo.Kindergarten;
 import edu.lits.maliatko.pojo.Queue;
-import edu.lits.maliatko.pojo.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QueueRepository extends CrudRepository<Queue, Integer> {
