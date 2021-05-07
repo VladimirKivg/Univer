78b01b1ee33296f38c5bd4bca59f700cae0ebd32
VladimirKing <cheremuha1@gmail.com>
2021-05-07 13:15:09 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/service/CreatingGroupsService.java
+++ src/main/java/edu/lits/maliatko/service/CreatingGroupsService.java
@@ -2,10 +2,8 @@ package edu.lits.maliatko.service;
 
 
 import edu.lits.maliatko.model.CreatingGroupsModel;
-import edu.lits.maliatko.pojo.Cluster;
 
 public interface CreatingGroupsService {
- //   void save(Cluster cluster);
 
     void save(CreatingGroupsModel creatingGroups);
 }
