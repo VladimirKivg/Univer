c0011c43dc8e9f4a83fbfdd511fc8845031448cc
VladimirKing <cheremuha1@gmail.com>
2021-05-07 13:15:32 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/model/ManagerGroupModel.java
+++ src/main/java/edu/lits/maliatko/model/ManagerGroupModel.java
@@ -1,12 +1,5 @@
 package edu.lits.maliatko.model;
 
-import edu.lits.maliatko.pojo.Child;
-import edu.lits.maliatko.pojo.Cluster;
-import edu.lits.maliatko.repository.ChildRepository;
-import edu.lits.maliatko.repository.ClusterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
 import java.util.ArrayList;
 import java.util.List;
 
