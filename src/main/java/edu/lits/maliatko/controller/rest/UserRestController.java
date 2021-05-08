2bc0381d88f4b47f12f394f0ba49d587420c04cf
VladimirKing <cheremuha1@gmail.com>
2021-05-08 22:51:45 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/controller/rest/UserRestController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/UserRestController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Set;
 
 @RestController
 public class UserRestController {
