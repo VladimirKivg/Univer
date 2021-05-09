760b1a3d7356387d09320b19fae28fcdbc8e1a82
VladimirKing <cheremuha1@gmail.com>
2021-05-09 15:36:11 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/controller/rest/RegChildRestController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/RegChildRestController.java
@@ -4,7 +4,6 @@ package edu.lits.maliatko.controller.rest;
 import edu.lits.maliatko.model.KidReg;
 import edu.lits.maliatko.service.RegistrationToKindergartenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
