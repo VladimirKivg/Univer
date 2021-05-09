0e66e391b3af33033fd4a318b44173feed51a004
VladimirKing <cheremuha1@gmail.com>
2021-05-09 15:37:10 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/controller/GroupController.java
+++ src/main/java/edu/lits/maliatko/controller/GroupController.java
@@ -4,7 +4,6 @@ import edu.lits.maliatko.model.ClusterModel;
 import edu.lits.maliatko.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
