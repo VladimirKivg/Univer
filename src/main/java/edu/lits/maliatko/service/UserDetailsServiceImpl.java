3ecdff0c26ba64c43ec5aa3abfb1fab7b5e8eb1c
VladimirKing <cheremuha1@gmail.com>
2021-05-09 15:36:18 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/service/UserDetailsServiceImpl.java
+++ src/main/java/edu/lits/maliatko/service/UserDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package edu.lits.maliatko.service;
 import edu.lits.maliatko.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
