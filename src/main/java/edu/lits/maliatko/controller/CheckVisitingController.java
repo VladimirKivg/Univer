0b36a6487b30900860b22636c1108977ed398935
VladimirKing <cheremuha1@gmail.com>
2021-05-10 14:43:25 +0300

    чистим код

--- src/main/java/edu/lits/maliatko/controller/CheckVisitingController.java
+++ src/main/java/edu/lits/maliatko/controller/CheckVisitingController.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
