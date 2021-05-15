cfc3fe1521dc59df7c30c2dfb28d2d10e1a8e848
VladimirKing <cheremuha1@gmail.com>
2021-05-15 11:38:52 +0300

    створюєм ще один рест контроллер

--- src/main/java/edu/lits/maliatko/controller/rest/CheckVisitingRestController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/CheckVisitingRestController.java
@@ -1,2 +1,30 @@
-package edu.lits.maliatko.controller.rest;public class CheckVisitingRestController {
+package edu.lits.maliatko.controller.rest;
+
+import edu.lits.maliatko.model.test.CheckVisitingModel;
+import edu.lits.maliatko.service.CheckVisitingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest")
+public class CheckVisitingRestController {
+
+    @Autowired
+    private CheckVisitingService checkVisitingService;
+
+    @RequestMapping("/visitingCheck")
+    public List<CheckVisitingModel> visiting() {
+        List<CheckVisitingModel> allByPresence1 = checkVisitingService.findAllByPresence(null);
+//
+//        model.addAttribute("attributeChildList2", allByPresence1);
+//
+//        model.addAttribute("content", "checkVisiting");
+        return allByPresence1;
+    }
+
+
 }
