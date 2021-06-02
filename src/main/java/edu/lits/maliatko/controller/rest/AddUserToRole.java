7f54d221436fca8dbaa9e3b60133ff6269b54bf3
VladimirKing <cheremuha1@gmail.com>
2021-06-02 17:50:55 +0300

    створюю добавлення юзерв в роль вихователем - ще не закінчино

--- src/main/java/edu/lits/maliatko/controller/rest/AddUserToRole.java
+++ src/main/java/edu/lits/maliatko/controller/rest/AddUserToRole.java
@@ -1,4 +1,14 @@
 package edu.lits.maliatko.controller.rest;
 
+import edu.lits.maliatko.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class AddUserToRole {
+
+    @Autowired
+    UserRepository userRepository;
+
 }
