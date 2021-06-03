ada3244a7d3ea5709a1c3db83008ad0f349f2c96
VladimirKing <cheremuha1@gmail.com>
2021-06-03 15:18:18 +0300

    створюю добавлення юзерв в роль вихователем - ще не закінчино

--- src/main/java/edu/lits/maliatko/repository/UserToRoleRepository.java
+++ src/main/java/edu/lits/maliatko/repository/UserToRoleRepository.java
@@ -1,10 +1,13 @@
 package edu.lits.maliatko.repository;
 
 
+import edu.lits.maliatko.pojo.User;
 import edu.lits.maliatko.pojo.UserToRole;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserToRoleRepository extends CrudRepository<UserToRole,Integer> {
     Iterable<UserToRole> findByRole_Role(String role);
     Iterable<UserToRole> findByRoleId(Integer roleId);
+
+    void sevUserToRole(User user);
 }
