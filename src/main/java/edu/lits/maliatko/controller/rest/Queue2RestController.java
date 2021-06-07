ebfb5f2528a5fd28b7e64dd622a8aa3765632a8e
VladimirKing <cheremuha1@gmail.com>
2021-06-08 02:05:30 +0300

    новий список дітей в групі

--- src/main/java/edu/lits/maliatko/controller/rest/Queue2RestController.java
+++ src/main/java/edu/lits/maliatko/controller/rest/Queue2RestController.java
@@ -1,2 +1,13 @@
-package edu.lits.maliatko.controller.rest;public class Queue2RestController {
+package edu.lits.maliatko.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class Queue2RestController {
+
+
+
+
 }
